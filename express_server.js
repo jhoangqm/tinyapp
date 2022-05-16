d6da35c084e2603c533e497a6eb4bc1399e85bc9
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-16 21:14:25 +0000

    Added redirection to '/u/:shortURL'

--- express_server.js
+++ express_server.js
@@ -35,22 +35,27 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {
+  let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls.json', (req, res) => {
