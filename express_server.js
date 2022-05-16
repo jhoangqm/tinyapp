61967bd8dd6c88c5415ed9ed0a828afb5c23acca
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-16 16:58:56 +0000

    Adding a second route and template

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render('urls_show', templateVars);
+});
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
