4b8b944b97f010dc253f4fbdb2ff983090649de0
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-17 13:54:44 +0000

    Added post login route, that sets cookie when using the login button

--- express_server.js
+++ express_server.js
@@ -95,7 +95,7 @@ app.post('/urls/:id', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username);
+  res.cookie('username', `${username}`);
   res.redirect('/urls');
 });
 
